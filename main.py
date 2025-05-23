c2ab7120b99a3c0f1ec79373fdaa67e4eef0eec6
Jevi <60514874+re-jevi@users.noreply.github.com>
2025-05-23 10:21:02 +0100

    Update main.py
    udpated to fix a few power of bugs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+MASK = (1 << 64) - 1          
+A, B, C = 221, 5, 0x123456   
+
 def boring_challenge(n: int, m: int) -> int:
-    return ((n + 221) * m + m + 5) * 0x123456 - m
+    return (((n + A) * m + m + B) * C - m) & MASK
 expectedVal = 458138870735950
 param1 = 12000019
 param2 = 32
